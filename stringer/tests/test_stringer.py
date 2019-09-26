ec3ffae01f0a9419a150739968206cbc4ef99763
billdback <billback@mac.com>
2019-09-26 11:03:34 -0700

    Fixed error in test cases.

--- stringer/tests/test_stringer.py
+++ stringer/tests/test_stringer.py
@@ -5,15 +5,15 @@ from stringer.stringer import Stringer
 class TestStringer(unittest.TestCase):
 
     def test_to_upper(self):
-        ostr = "This is a mixed case string."
+        ostr = "This is a mixed case string"
         s = Stringer()
         ustr = s.to_upper(ostr)
 
         self.assertEqual("THIS IS A MIXED CASE STRING", ustr)
 
     def test_to_lower(self):
-        ostr = "This is a mixed case string."
+        ostr = "This is a mixed case string"
         s = Stringer()
-        lstr = s.to_upper(ostr)
+        lstr = s.to_lower(ostr)
 
         self.assertEqual("this is a mixed case string", lstr)
